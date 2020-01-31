3eb8ad0c520a7bf0d6f11c0cdd97e12c17ca6a8e
Khayo Vannucci <khayovannucci@gmail.com>
2020-01-30 23:54:28 -0300

    1043 - Triangle (iniciado)

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//1043 - Triangle
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] vs = input.Split(' ');
+
+            double N1 = double.Parse(vs[0]);
+            double N2 = double.Parse(vs[1]);
+            double N3 = double.Parse(vs[2]);
+        }
+    }
+}
