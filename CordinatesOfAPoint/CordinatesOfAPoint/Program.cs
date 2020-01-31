a5bbe9232b3e30d642f1fa5ed9a31e3557e4d47a
Khayo Vannucci <khayovannucci@gmail.com>
2020-01-30 23:12:34 -0300

    1041 - Coprdinates of a Point (concluido)

---
+++ CordinatesOfAPoint/CordinatesOfAPoint/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace CordinatesOfAPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coord = Console.ReadLine();
+            string[] vs = coord.Split(' ');
+
+            double x = double.Parse(vs[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(vs[1], CultureInfo.InvariantCulture);
+
+            if (x == 0.0 || y == 0.0)
+            {
+                if (x != 0)
+                {
+                    Console.WriteLine("Eixo Y");
+                }
+                else if (y != 0)
+                {
+                    Console.WriteLine("Eixo X");
+                }
+                else
+                {
+                    Console.WriteLine("Origem");
+                }
+            }
+            else if (x > 0.0 &&  y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0 )
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
