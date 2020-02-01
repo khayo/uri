0ad5004b28827813ab58350497919a555ab760df
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-01 18:46:55 -0300

    1046 - Game Time (complete)

---
+++ GameTime/GameTime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//1046 - Game Time
+
+namespace GameTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] vs = input.Split(' ');
+            int start = int.Parse(vs[0]);
+            int end = int.Parse(vs[1]);
+            int total = 0;
+
+            if (start >= end)
+            {
+                total = (24 - start) + end;
+            }
+            else
+            {
+                total = end - start;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {total} HORA(S)");
+
+
+        }
+    }
+}
