19278a5dc99c03c6b0c57f754fc3488eed89029a
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-06 20:00:11 -0300

    1060 - Positive numbers

---
+++ PositiveNumber/PositiveNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace PositiveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputs = new double[6];
+            int qtd = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                inputs[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (inputs[i] > 0)
+                {
+                    qtd++;
+                }
+            }
+
+            Console.WriteLine(qtd + " valores positivos");
+        }
+    }
+}
