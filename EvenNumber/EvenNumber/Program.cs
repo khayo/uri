f0943483280ced25e672fe8d36bcd2c5770b5c2c
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-05 21:36:44 -0300

    1059 - Even Number (Complete)

---
+++ EvenNumber/EvenNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    //1059 - Even Number
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
