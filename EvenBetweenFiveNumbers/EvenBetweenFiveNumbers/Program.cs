967d75e2d2dcea19d10421722abf92566d941202
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-25 20:11:45 -0300

    1065 - Even Between Five Numbers (complete)

---
+++ EvenBetweenFiveNumbers/EvenBetweenFiveNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// 1065 - Even Between Five Numbers
+
+namespace EvenBetweenFiveNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            int even = 0;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (num[i] % 2 == 0)
+                {
+                    even++;                   
+                }
+
+            }
+
+            Console.WriteLine(even + " valores pares");            
+        }
+    }
+}
