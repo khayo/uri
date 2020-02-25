f289c28cd8884eb77c08e95c7d00b9d5c0bc7336
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-25 20:06:05 -0300

    1064 - Positive And Average (complete)

---
+++ PositiveAndAverage/PositiveAndAverage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+//1064 - PositiveAndAverage
+
+namespace PositiveAndAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] num = new double[6];
+            int pos = 0;
+            double average = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                num[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (num[i] >= 0)
+                {
+                    pos++;
+                    average += num[i];
+                }
+
+                
+
+            }
+
+            average = average / pos;
+
+            Console.WriteLine(pos + " valores positivos");
+            Console.WriteLine(average.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
