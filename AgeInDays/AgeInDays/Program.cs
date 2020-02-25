d2c70c94c7e5d630edb372b782a4f72e8492692f
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-25 10:43:17 -0300

    1020 - Age in Days (complete)

---
+++ AgeInDays/AgeInDays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AgeInDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDays = int.Parse(Console.ReadLine());
+
+            int years = totalDays / 365;
+            int aux = totalDays % 365;
+            int months = aux / 30;
+            int days = aux % 30;
+
+            Console.WriteLine(years + " ano(s)");
+            Console.WriteLine(months + " mes(es)");
+            Console.WriteLine(days + " dia(s)");
+        }
+    }
+}
