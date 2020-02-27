1c6584a4bc9fc0f2aca7239707130f24b081a0c3
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-26 21:56:45 -0300

    1067 - Odd Number (complete)

---
+++ OddNumber/OddNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 1067 - Odd Number
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x >= 1 && x <= 1000)
+            {
+                for (int i = 0; i <= x; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
