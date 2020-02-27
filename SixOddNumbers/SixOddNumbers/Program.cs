4dd4f8679c75f8b522fef2051302835f2ac4e2f5
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-26 22:03:45 -0300

    1070 - Six Odd Numbers (complete)

---
+++ SixOddNumbers/SixOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// 1070 - Six Odd Numbers
+
+namespace SixOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x > 0)
+            {
+                for (int i = x; i < x + 12; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
