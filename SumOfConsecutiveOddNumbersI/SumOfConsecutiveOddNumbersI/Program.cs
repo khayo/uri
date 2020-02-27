78ba6e4a856cdb855743535f1400f6524dbb9ca0
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-26 23:12:23 -0300

    1071 - Sum of Consecutive Odd Numbers I (complete)

---
+++ SumOfConsecutiveOddNumbersI/SumOfConsecutiveOddNumbersI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// 1071 - Sum of Consecutive Odd Numbers I
+
+namespace SumOfConsecutiveOddNumbersI
+{
+    class Program
+    {              
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            if (x >= y)
+            {
+                for (int i = y + 1; i < x; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = x + 1; i < y; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += i;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
