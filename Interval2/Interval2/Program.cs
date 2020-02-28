a6bb905c95a6c1566242d9d0b539af067a626a0d
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-27 22:31:42 -0300

    1072 - Interval 2 (complete)

---
+++ Interval2/Interval2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 1072 - Inteval 2
+
+namespace Interval2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            int inside = 0;
+            int outside = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var item in list)
+            {
+                if (item >= 10 && item <= 20)
+                {
+                    inside++;
+                }
+                else
+                {
+                    outside++;
+                }
+            }
+
+            Console.WriteLine(inside + " in");
+            Console.WriteLine(outside + " out");
+        }
+    }
+}
