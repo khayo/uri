9c9604698600fcb93a90ddd446ce67e75830e275
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-27 22:38:35 -0300

    1073 - Even Square (complete)

---
+++ EvenSquare/EvenSquare/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 1073 - Even Square
+
+namespace EvenSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int aux = i * i;
+                    Console.WriteLine($"{i}^2 = {aux}");                    
+                }
+            }
+        }
+    }
+}
