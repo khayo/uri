2f00c77dbe913487923a2da82f6c699fbf5bc4cc
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-28 19:17:27 -0300

    1078 - Multiplication Table (complete)

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// 1078 - Multiplication Table
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 1 && n < 1000)
+            {
+                for (int i = 1; i <= 10 ; i++)
+                {
+                    Console.WriteLine($"{i} x {n} = {i * n}");
+                }
+            }
+        }
+    }
+}
