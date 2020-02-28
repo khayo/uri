d092cbe576b00c54f8b5aed716ae942cedad1a69
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-27 22:47:24 -0300

    1074 - Even or Odd (complete)

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 1074 - Even Or Odd
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var item in list)
+            {
+                if (item == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+                else if (item > 0)
+                {
+                    if (item % 2 == 0)
+                    {
+                        Console.WriteLine("EVEN POSITIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ODD POSITIVE");
+                    }
+                }
+                else
+                {
+                    if (item % 2 == 0)
+                    {
+                        Console.WriteLine("EVEN NEGATIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ODD NEGATIVE");
+                    }
+                }
+            }
+        }
+    }
+}
