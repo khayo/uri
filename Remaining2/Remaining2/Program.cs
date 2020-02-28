7abb626ce05377c2ee8f6077bedc4a9422925887
Khayo Vannucci <khayovannucci@gmail.com>
2020-02-27 22:51:55 -0300

    1075 - Remaining 2 (complete)

---
+++ Remaining2/Remaining2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 1075 - Remaining 2
+
+namespace Remaining2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i % n == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
