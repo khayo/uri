3a175da5a8921507c3ac495f25afdd3f12de2bf6
Khayo Vannucci <khayovannucci@gmail.com>
2020-03-01 20:47:40 -0300

    1079 - Weigted Averages (complete)

---
+++ WeightedAverages/WeightedAverages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+// 1079 - Weighed Averages
+
+namespace WeightedAverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[,] values = new double[n, 3];
+
+            for (int i = 0; i < n; i++)
+            {
+                string num = Console.ReadLine();
+                string[] vs = num.Split(' ');
+                for (int j = 0; j < 3; j++)
+                {
+                    values[i,j] = double.Parse(vs[j], CultureInfo.InvariantCulture);
+                }
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                double avg = ((values[i, 0] * 2) + (values[i, 1] * 3) + (values[i, 2] * 5)) / 10;
+                Console.WriteLine(avg.ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
