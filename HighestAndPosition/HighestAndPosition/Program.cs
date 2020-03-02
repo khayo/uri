f4521c994070494d031cc945b56a30c4add7a81d
Khayo Vannucci <khayovannucci@gmail.com>
2020-03-01 21:01:41 -0300

    1080 - Highest and Position (complete)

---
+++ HighestAndPosition/HighestAndPosition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 1080 - Highest and Position
+
+namespace HighestAndPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] values = new int[101];
+
+            int highest = 0;
+
+            int position = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                values[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (values[i] > values[position])
+                {
+                    highest = values[i];
+                    position = i;
+                }
+            }
+
+            Console.WriteLine(highest);
+            Console.WriteLine(position);
+        }
+    }
+}
