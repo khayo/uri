19417ae27f5b0e95bd57efb56df50f7a32a7ba31
Khayo Vannucci <khayovannucci@gmail.com>
2020-03-02 21:15:10 -0300

    1096 - Sequence IJ 2 (cemplete)

---
+++ SequenceIJ2/SequenceIJ2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 1096 - Sequence IJ 2
+
+namespace SequenceIJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i+=2)
+            {
+                for (int j = 7; j >= 5; j--)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                }
+            }
+        }
+    }
+}
