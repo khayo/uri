8b1ea25f14765fa5661450618ad4cb46f1aa2b58
Khayo Vannucci <khayovannucci@gmail.com>
2020-03-02 21:10:33 -0300

    1095 - Sequence IJ 1 (Complete)

---
+++ SequenceIJ1/SequenceIJ1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 1095 - Sequence IJ 1
+
+namespace SequenceIJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 60;
+            while (j >= 0) 
+            {
+                Console.WriteLine("I=" + i + " J=" + j);
+                
+
+                i += 3;
+                j -= 5;
+            }
+        }
+    }
+}
