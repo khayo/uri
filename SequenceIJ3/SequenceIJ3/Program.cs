b7285848f7b629a89b3134c6dcb2b42a6d6db811
Khayo Vannucci <khayovannucci@gmail.com>
2020-03-02 21:33:35 -0300

    1097 - Sequence IJ 3 (complete)

---
+++ SequenceIJ3/SequenceIJ3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// 1097 - Sequence IJ 3
+
+namespace SequenceIJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 1; i <= 9; i+=2)
+            {                
+                for (int j = n; j >= n - 2; j--)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                }
+                n += 2;
+            }
+        }
+    }
+}
