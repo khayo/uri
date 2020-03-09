f7ad12537875cc548967141bf3f05d3f3e349549
Khayo <khayovannucci@gmail.com>
2020-03-08 23:16:18 -0300

    1113 - Ascendig And Descending (complete)

---
+++ AscendingAndDescending/AscendingAndDescending/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 1113 - Ascending and Descending
+
+namespace AscendingAndDescending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int x = 0;
+            int y = 0;
+            do
+            {
+                string values = Console.ReadLine();
+                string[] numbers = values.Split(' ');
+                x = int.Parse(numbers[0]);
+                y = int.Parse(numbers[1]);
+                if (x < y)
+                {
+                    list.Add("Crescente");
+                }
+                else if(x > y)
+                {
+                    list.Add("Decrescente");
+                }
+
+            } while (x != y);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
