63134493147d4d3a9b8ed903d1fd10eb836bcdfb
Khayo <khayovannucci@gmail.com>
2020-03-29 18:26:28 -0300

    1114 - Fixed Password (complete)

---
+++ FixedPassword/FixedPassword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+//1114 - Fixed Password
+
+namespace FixedPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pass = 2002;
+            int input;
+            do
+            {
+                 input = int.Parse(Console.ReadLine());
+                if (pass != input)
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+                else
+                {
+                    Console.WriteLine("Acesso Permitido");
+                }
+
+            } while (pass != input );
+        }
+    }
+}
