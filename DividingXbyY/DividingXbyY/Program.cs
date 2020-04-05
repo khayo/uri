ecc11e5874c3289af4b68e1a4322e03f0cea67a0
Khayo <khayovannucci@gmail.com>
2020-04-04 21:21:50 -0300

    1116 - Dividing X by Y (Complete)

---
+++ DividingXbyY/DividingXbyY/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+// 1116 - Dividing X by Y
+
+namespace DividingXbyY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[n, 2];
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] vs = input.Split(' ');
+                matrix[i, 0] = vs[0];
+                matrix[i, 1] = vs[1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = double.Parse(matrix[i, 0]);
+                double y = double.Parse(matrix[i, 1]);
+                if (y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double total = x / y;
+                    Console.WriteLine(total.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
