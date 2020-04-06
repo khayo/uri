b36bac4ad600450824b45f3a03f50aa37400cc3f
Khayo <khayovannucci@gmail.com>
2020-04-05 22:53:31 -0300

    1133 - Rest of a Division (complete)

---
+++ RestOfaDivision/RestOfaDivision/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestOfaDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                int aux = y;
+                y = x;
+                x = aux;
+            }
+
+            for (int i = x+1; i < y; i++)
+            {
+
+                int aux = i % 5;
+
+                if (aux == 2 || aux == 3)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+    }
+}
