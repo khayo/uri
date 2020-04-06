73327d67bc5671f466a94578c32947abf7b8f37a
Khayo <khayovannucci@gmail.com>
2020-04-05 23:20:01 -0300

    1134 - Types oF Fuel (complete)

---
+++ TypeOfFuel/TypeOfFuel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// 1134 - Type of Diesel
+
+namespace TypeOfFuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod = 0, alc = 0, gas = 0, die = 0; //ainda está feio!
+
+            while (cod != 4)
+            {
+                cod = int.Parse(Console.ReadLine());
+
+                switch (cod)
+                {
+                    case 1:
+                        alc++;
+                        break;
+                    case 2:
+                        gas++;
+                        break;
+                    case 3:
+                        die++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alc);
+            Console.WriteLine("Gasolina: " + gas);
+            Console.WriteLine("Diesel: " + die);
+        }
+    }
+}
