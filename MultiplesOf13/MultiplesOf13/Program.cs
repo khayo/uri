5cf510bc580cec4f751d1d2be130c1dc21bba32d
Khayo <khayovannucci@gmail.com>
2020-04-05 22:43:08 -0300

    1133 - Multiples of 13 (cemplete)

---
+++ MultiplesOf13/MultiplesOf13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Multiples of 13
+
+namespace MultiplesOf13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            if (x < y)
+            {
+                int aux = y;
+                y = x;
+                x = aux;
+            }
+
+            for (int i = y; i <= x; i++)
+            {
+                int aux = i % 13;
+                if (aux != 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
