7b2c24b9bbf98dcc2717bb6451159babedc8e7d4
Khayo <khayovannucci@gmail.com>
2020-04-05 22:56:06 -0300

    1133 - Rest of a Division (removed unused library)

--- RestOfaDivision/RestOfaDivision/Program.cs
+++ RestOfaDivision/RestOfaDivision/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// 1133 - Rest of a Division
 
 namespace RestOfaDivision
 {
