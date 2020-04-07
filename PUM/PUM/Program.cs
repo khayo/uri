dd48d6a7df216aeca4deca1488c94c8b9ca48caa
Khayo <khayovannucci@gmail.com>
2020-04-07 20:41:47 -0300

    1142 - PUM (Complete)

---
+++ PUM/PUM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// 1142 - PUM
+
+namespace PUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 1;
+
+            for (int i = 0; i < n; i++)
+            {   
+                for (int j = 0; j < 4; j++)
+                {
+                    if (counter % 4 == 0)
+                    {
+                        Console.WriteLine("PUM");
+                    }
+                    else
+                    {
+                        Console.Write(counter + " ");
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+}
