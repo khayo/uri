6fc2a3a4d58fead7b08ad7abe5b754c2666c3902
Khayo <khayovannucci@gmail.com>
2020-04-07 21:23:55 -0300

    1143 - Squared end Cubic (presentetion error)

---
+++ SquareAndCubic/SquareAndCubic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// 1143 - Squared and Cubic
+
+namespace SquareAndCubic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double value = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                value = i;
+                for (double j = 1; j <= 3; j++)
+                {
+                    value = Math.Pow(i, j);
+                    Console.Write(value + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
