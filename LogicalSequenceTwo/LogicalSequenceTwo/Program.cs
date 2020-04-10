e305b8885495fae30032e69293c566bd1b14cfc5
Khayo <khayovannucci@gmail.com>
2020-04-10 08:47:16 -0300

    1145 - Logica Sequence 2 (Complete)

---
+++ LogicalSequenceTwo/LogicalSequenceTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// 1145 - Logical Sequence 2
+
+namespace LogicalSequenceTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] vs = input.Split(' ');
+            int x = int.Parse(vs[0]);
+            int y = int.Parse(vs[1]);
+
+            for (int i = 1; i <= y; i++)
+            {
+                Console.Write(i);
+                if (i % x == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
