ff06bc3c4ecc9cc3870f9cbbf78ccccd596414bd
Khayo <khayovannucci@gmail.com>
2020-04-10 08:39:12 -0300

    1144 - Logical Sequence (Complete)

---
+++ LogicalSequence/LogicalSequence/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// 1144 - Logical Sequence
+
+namespace LogicalSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double value = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                value = i;
+                for (int k = 0; k < 2; k++)
+                {
+                    for (double j = 1; j <= 3; j++)
+                    {
+                        if (k != 0 && j != 1)
+                        {
+                            value = 1 + (Math.Pow(i, j));
+                        }
+                        else
+                        {
+                            value = Math.Pow(i, j);
+                        }
+                        Console.Write(value);
+                        if (j != 3)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
