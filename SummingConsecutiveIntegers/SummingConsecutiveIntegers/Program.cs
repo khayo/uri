0090a7086eaa058c61fd3d77f46de99f3bf646f4
Khayo <khayovannucci@gmail.com>
2020-04-11 23:58:06 -0300

    1149 - Summing Consecutive Integers (Complete)

---
+++ SummingConsecutiveIntegers/SummingConsecutiveIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// 1149 - Summing Consecutive Integers
+
+namespace SummingConsecutiveIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int n = 0;
+            int sum = 0;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (int.Parse(input[i]) > 0)
+                {
+                    n = int.Parse(input[i]);
+                }                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum += (a+i);
+            }            
+            Console.WriteLine(sum);
+        }
+    }
+}
+
