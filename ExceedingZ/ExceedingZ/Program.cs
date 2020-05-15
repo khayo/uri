e43ff75d5e31f38f35cbad0a26f36394e422d297
Khayo <khayovannucci@gmail.com>
2020-05-14 21:07:16 -0300

    1150 - Exceeding Z (complete)

---
+++ ExceedingZ/ExceedingZ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+// 1150 - Exceeding Z
+
+namespace ExceedingZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int z;
+            int aux;
+            int count = 1;
+
+            do
+            {
+                z = int.Parse(Console.ReadLine());
+            } while (z <= x);
+
+            aux = x;
+
+            for (int i = 1; z > aux; i++)
+            {
+                aux += x + i;
+                count++;
+            }
+            
+            Console.WriteLine(count);
+        
+        }
+    }
+}
